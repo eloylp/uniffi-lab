e28a3338458d867b18b41acfec279fdfef3a8cb8
eloylp <eloylp@eloylp.dev>
2023-02-17 18:12:54 +0100

    Add error handling to basic example

--- basic/src/lib.rs
+++ basic/src/lib.rs
@@ -1,5 +1,14 @@
-pub fn add(a: u32, b: u32) -> u32 {
-    a + b
+type Result<T, E = ArithmeticError> = std::result::Result<T, E>;
+
+#[derive(Debug, thiserror::Error)]
+pub enum ArithmeticError {
+    #[error("Integer overflow for {a} and {b}")]
+    IntegerOverflow { a: u32, b: u32 },
+}
+
+pub fn add(a: u32, b: u32) -> Result<u32, ArithmeticError> {
+    a.checked_add(b)
+        .ok_or(ArithmeticError::IntegerOverflow { a: a, b: b })
 }
 
 uniffi::include_scaffolding!("basic");
--- basic/tests/python/test.py
+++ basic/tests/python/test.py
@@ -4,6 +4,11 @@ from basic import *
 class Test(unittest.TestCase):
     def test_add(self):
         self.assertEqual(6, add(2, 4))
+    def test_add_errors_overflow(self):
+        with self.assertRaises(ArithmeticError.IntegerOverflow) as context:
+            add(4294967295, 1)
+        self.assertEquals(4294967295, context.exception.a)
+        self.assertEquals(1, context.exception.b)
 
 if __name__ == '__main__':
     unittest.main()
