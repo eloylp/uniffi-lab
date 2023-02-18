ec19967f951a42a133c3adfcf68800693c87e57e
eloylp <eloylp@eloylp.dev>
2023-02-18 19:19:42 +0100

    Use non deprecated python test assertions

--- basic/tests/python/test.py
+++ basic/tests/python/test.py
@@ -7,8 +7,8 @@ class Test(unittest.TestCase):
     def test_add_errors_overflow(self):
         with self.assertRaises(ArithmeticError.IntegerOverflow) as context:
             add(4294967295, 1)
-        self.assertEquals(4294967295, context.exception.a)
-        self.assertEquals(1, context.exception.b)
+        self.assertEqual(4294967295, context.exception.a)
+        self.assertEqual(1, context.exception.b)
 
 if __name__ == '__main__':
     unittest.main()
